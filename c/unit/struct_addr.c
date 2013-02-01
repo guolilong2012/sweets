9fef6edd5be3667ef69d5ecbca97f713bc07e526
Guo lilong <guolilong2012@sohu.com>
2013-02-01 11:18:41 +0800

    fun

---
+++ c/unit/struct_addr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define offsetof(TYPE, MEMBER) ((unsigned int)(&((TYPE *)0)->MEMBER))
+#define struct_addr(MEMBER_ADDR, TYPE, MEMBER) \
+		(((unsigned int)MEMBER_ADDR) - (offsetof(TYPE, MEMBER)))
+
+typedef struct {
+	int a;
+	int b;
+} TEST;
+
+int main(void)
+{
+	TEST test;
+
+	//printf("%d\n", *(int *)0); //segment error
+	//printf("%d\n", ((TEST *)0)->b); //segment error
+	printf("%p\n", &((TEST *)0)->b); //ok
+	printf("offsetof(TEST, b) = <%u>\n", offsetof(TEST, b));
+	printf("addr(test) = <%p>\n", &test);
+	printf("struct_addr(&test->b, TEST, b) = <%p>\n", \
+		(void *)struct_addr(&test.b, TEST, b));
+
+	return 0;
+}
